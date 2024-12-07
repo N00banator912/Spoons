5a9ba06c7bbc72513aadf696ffc612c99d7c5041
Kat Brown <xxN00banator912xx@gmail.com>
2024-12-07 14:40:28 -0500

    Added Fonts

--- Script/player.gd
+++ Script/player.gd
@@ -5,6 +5,25 @@ const SPEED = 200.0
 
 var isCrouch = false
 
+var gold = 10
+
+var baseMaxSpoons
+var maxSpoons = 6		# Spoons will be doubled to handle the partially broken state
+var spoons = 1			# Player starts with 1 broken spoon
+
+# Mood Variables
+var houseKeepingBonus = 3
+var houseCleaned = false
+
+var averageFoodQuality = 1.0
+var eatenToday = false
+
+var exerciseBonus = 4
+var hasExercised = false
+
+var passoutPenalty = -2
+var playerPassedOut = false
+
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hurt_box_coll: CollisionShape2D = $HurtBox/CollisionShape2D
 
@@ -51,3 +70,30 @@ func _on_animated_sprite_2d_animation_finished() -> void:
 #func _input(event):
 #	if event.is_action_pressed("action"):
 #		sprite.play("crouch")
+
+func Sleep() -> void:
+	maxSpoons = baseMaxSpoons
+	if (houseCleaned):
+		maxSpoons += houseKeepingBonus
+	if (hasExercised):
+		maxSpoons += exerciseBonus
+	if (playerPassedOut):
+		maxSpoons += passoutPenalty
+	
+		
+	if (eatenToday):
+		maxSpoons += floor(averageFoodQuality)
+	else:
+		maxSpoons = floor(maxSpoons / 2)
+		
+	if (maxSpoons >= 10): # Trigger End Game
+		print_debug("Congrations, Ya Done It!")
+		
+	elif (maxSpoons <= 0): # The Player Doesn't Wake Up at All
+		Die()
+		
+	else: 		# Start Next Day
+		spoons = maxSpoons
+		
+func Die():
+	print_debug("Oops, you died")
