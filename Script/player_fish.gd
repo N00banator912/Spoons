ccf3390973ca42a212af54587609909e39ae11e2
Sarah Brown <sarah.brown.dev@gmail.com>
2024-12-07 14:40:15 -0500

    Fish now spawn!
    Up to 5 fish spawn. Need to work on the mechanics of actually, like, catching the fish lol

---
+++ Script/player_fish.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+@onready var hitbox: CollisionShape2D = $Hitbox/CollisionShape2D
+
+const SPEED = 200.0
+
+var screen_size
+
+signal hit
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_vector("left","right","up","down")
+
+	velocity = direction * SPEED
+
+
+	move_and_slide()
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	#hide()
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	hit.emit()
+	hitbox.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	hitbox.disabled = false
